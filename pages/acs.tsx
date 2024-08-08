2f4dfe7bad52b81d5341cb984c39b0e6dd85a368
RamiroPadro <ramirop@shifta.la>
2024-08-07 22:51:25 -0300

    Se cargan tablas de perdidas para sac

--- pages/acs.tsx
+++ pages/acs.tsx
@@ -57,6 +57,18 @@ const Acs = () => {
     };
   };
 
+  const calcularTotales = (index: number) => {
+    return shipsCosts.reduce(
+      (acc, ship) => {
+        acc.metal += ship.metal * cantidad[ship.tipo][index];
+        acc.cristal += ship.cristal * cantidad[ship.tipo][index];
+        acc.deuterio += ship.deuterio * cantidad[ship.tipo][index];
+        return acc;
+      },
+      { metal: 0, cristal: 0, deuterio: 0 }
+    );
+  };
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Calculadora de Repartidor SAC</h1>
@@ -86,72 +98,107 @@ const Acs = () => {
               type="text"
               value={nombre}
               onChange={(e) => handleNombreChange(index, e.target.value)}
-              className="border p-2 rounded w-full"
+              className="border p-2 rounded-md w-1/4"
             />
           </div>
         ))}
       </div>
 
-      <h2 className="text-xl font-bold mb-4">Pérdidas</h2>
-      <div>
-        {nombresJugadores.map((nombre, index) => (
-          <div key={index} className="mb-4">
-            <h3 className="text-lg font-semibold mb-2">{nombre}</h3>
-            <div className="overflow-x-auto">
-              <table className="table-auto w-full border-collapse border border-gray-300">
-                <thead>
-                  <tr>
-                    <th className="border border-gray-300 p-2 w-1/6">Tipo</th>
-                    <th className="border border-gray-300 p-2 w-1/6">
-                      Cantidad
-                    </th>
-                    <th className="border border-gray-300 p-2 w-1/6">Metal</th>
-                    <th className="border border-gray-300 p-2 w-1/6">
-                      Cristal
-                    </th>
-                    <th className="border border-gray-300 p-2 w-1/6">
-                      Deuterio
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {shipsCosts.map((ship) => (
-                    <tr key={ship.tipo}>
-                      <td className="border border-gray-300 p-2">
-                        {ship.tipo}
-                      </td>
-                      <td className="border border-gray-300 p-2">
-                        <input
-                          type="number"
-                          min={0}
-                          value={cantidad[ship.tipo][index]}
-                          onChange={(e) =>
-                            handleCantidadChange(
-                              ship.tipo,
-                              index,
-                              e.target.value
-                            )
-                          }
-                          className="border p-2 rounded w-full"
-                        />
-                      </td>
-                      <td className="border border-gray-300 p-2">
-                        {calcularCosto(ship, index).metal.toLocaleString()}
-                      </td>
-                      <td className="border border-gray-300 p-2">
-                        {calcularCosto(ship, index).cristal.toLocaleString()}
-                      </td>
-                      <td className="border border-gray-300 p-2">
-                        {calcularCosto(ship, index).deuterio.toLocaleString()}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+      {nombresJugadores.length > 0 &&
+        nombresJugadores[0] !== '' &&
+        nombresJugadores[1] !== '' && (
+          <>
+            <h2 className="text-xl font-bold mb-4 uppercase mt-10">Pérdidas</h2>
+            <div>
+              {nombresJugadores.map((nombre, index) => (
+                <div key={index} className="mb-16">
+                  <hr className='border-2 border-black mb-10'/>
+                  <h3 className="text-lg font-bold mb-2">Pérdidas de {nombre}</h3>
+                  <div className="overflow-x-auto">
+                    <table className="table-auto w-full border-collapse border border-gray-300">
+                      <thead>
+                        <tr>
+                          <th className="border border-gray-300 p-2 w-1/6">
+                            Tipo
+                          </th>
+                          <th className="border border-gray-300 p-2 w-1/6">
+                            Cantidad
+                          </th>
+                          <th className="border border-gray-300 p-2 w-1/6">
+                            Metal
+                          </th>
+                          <th className="border border-gray-300 p-2 w-1/6">
+                            Cristal
+                          </th>
+                          <th className="border border-gray-300 p-2 w-1/6">
+                            Deuterio
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {shipsCosts.map((ship) => (
+                          <tr key={ship.tipo}>
+                            <td className="border border-gray-300 p-2">
+                              {ship.tipo}
+                            </td>
+                            <td className="border border-gray-300 p-2">
+                              <input
+                                type="number"
+                                min={0}
+                                value={cantidad[ship.tipo][index]}
+                                onChange={(e) =>
+                                  handleCantidadChange(
+                                    ship.tipo,
+                                    index,
+                                    e.target.value
+                                  )
+                                }
+                                className="border p-2 rounded w-full"
+                              />
+                            </td>
+                            <td className="border border-gray-300 p-2">
+                              {calcularCosto(
+                                ship,
+                                index
+                              ).metal.toLocaleString()}
+                            </td>
+                            <td className="border border-gray-300 p-2">
+                              {calcularCosto(
+                                ship,
+                                index
+                              ).cristal.toLocaleString()}
+                            </td>
+                            <td className="border border-gray-300 p-2">
+                              {calcularCosto(
+                                ship,
+                                index
+                              ).deuterio.toLocaleString()}
+                            </td>
+                          </tr>
+                        ))}
+                        <tr>
+                          <td className="border border-gray-300 p-2 font-bold">
+                            Total {nombre}
+                          </td>
+                          <td className="border border-gray-300 p-2"></td>
+                          <td className="border border-gray-300 p-2">
+                            {calcularTotales(index).metal.toLocaleString()}
+                          </td>
+                          <td className="border border-gray-300 p-2">
+                            {calcularTotales(index).cristal.toLocaleString()}
+                          </td>
+                          <td className="border border-gray-300 p-2">
+                            {calcularTotales(index).deuterio.toLocaleString()}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              ))}
             </div>
-          </div>
-        ))}
-      </div>
+          </>
+        )}
     </div>
   );
 };
