af0563095a9688d69e949984777c19244ffaa10d
RamiroPadro <ramirop@shifta.la>
2024-08-09 18:12:55 -0300

    Auth

--- components/auth/ProtectedRoute.tsx
+++ components/auth/ProtectedRoute.tsx
@@ -1,20 +1,24 @@
 // components/ProtectedRoute.tsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUserContext } from '@/context/userContext';
 import { useRouter } from 'next/router';
 
-const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  const { password } = useUserContext();
-  const router = useRouter();
-
-  if (!password || password == '') {
-    router.push('/');
-    return null;
-  }
-
-  return <>{children}</>;
-};
-
-export default ProtectedRoute;
+const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+	const { password } = useUserContext();
+	const router = useRouter();
+	const [loading, setLoading] = useState(true);
+  
+	useEffect(() => {
+	  if (!password || password === '') {
+		router.push('/'); // Redirige al login si no hay contraseña
+	  } else {
+		setLoading(false); // Permite que el contenido se muestre si la contraseña es correcta
+	  }
+	}, [password, router]);
+  
+	if (loading) return <div>Loading...</div>; // Opcional: Puedes mostrar un estado de carga mientras se verifica la autenticación
+  
+	return <>{children}</>;
+  };
+  
+  export default ProtectedRoute;
